208140af0282c3bc967d81f378f46fe832ea9b8e
boranozum <62467715+boranozum@users.noreply.github.com>
2020-03-22 22:12:30 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -21,33 +21,32 @@ void loop_continue(char x){
 int main(){
     
     int b,start[255],end[255],step[255],i = 0,m,k=1;
-    char normal[255],value[255];
+    char normalchar[255];
     
    do {
         
-        normal[i] = getchar();
+        normalchar[i] = getchar();
         
         scanf("%d %d %d",&start[i],&end[i],&step[i]);
         
         if ((start[i] > end[i] && step[i] > 0) || (start[i] < end[i] && step[i] < 0)) goto stop;
         
         else {
-            normal[i] = value[i];
-            value[i] = start[i];
+            normalchar[i] = start[i];
         }
         i++;
         
     }
     while(b = getchar() != EOF);
     
-    while(value[0] < end[0]){
+    while(normalchar[0] < end[0]){
         
-        printf("%c : %d %c : %d %c : %d\n",normal[0],value[0],normal[1],value[1],normal[2],value[2]);
-        value[i-1] += step[i-1];
+        printf("%c : %d %c : %d %c %d\n",normalchar[0],normalchar[0],normalchar[1],normalchar[1],normalchar[2],normalchar[2]);
+        normalchar[i-1] += step[i-1];
         for (m=i-1;m > 0;m--){
-            if (value[m] > end[m]){
-                value[m-1] += step[m-1];
-                value[m] = start[m];
+            if (normalchar[m] > end[m]){
+                normalchar[m-1] += step[m-1];
+                normalchar[m] = start[m];
             }
         }
     }
